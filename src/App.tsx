6f21c68e84a7f5029cb859b5cf11dd13d473c7f5
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-06-10 19:33:51 -0300

    Feat: Componente Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import styles from "./app.module.scss";
+import Footer from "./components/Footer";
 import SquadSection from './components/SquadSection';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className={styles.container}>
       <SquadSection />
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,26 @@
+import styles from "./Footer.module.scss";
+import { FaSpotify, FaYoutube, FaLinkedin } from "react-icons/fa";
+import dentinho from "../../../public/dentinho.png";
+import pipoca from "../../../public/pipoca_agil.png";
+import urso from "../../../public/logo_pipoca 1.png";
+
+
+export default function Footer(){
+  return (
+    <footer className={styles.container}>
+      <div className={styles.container__esquerda}>
+        <img src={urso} alt="Imagem de um urso" />
+        <img src={pipoca} alt="Logo do Pipoca Ágil" />
+      </div>
+      <div className={styles.container__central}>
+        <div>
+          <FaSpotify size={57} color="#1DB954"/>
+          <FaYoutube size={57} color="red"/>
+          <FaLinkedin size={57} color="blue"/>
+        </div>
+        <p>&copy; 2003 - 2024 Todos os direitos reservados.</p>
+      </div>
+      <img src={dentinho} alt="" />
+    </footer>
+  )
+}
