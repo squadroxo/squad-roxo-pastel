1a7b053f52e4cd7b11d79115ad5d097d9ae9dc56
Daniela Alves <danieladossantosalves01@gmail.com>
2024-08-05 18:44:18 -0300

    fix: footer logos

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,5 +1,7 @@
 import styles from "./Footer.module.scss";
-import { FaSpotify, FaYoutube, FaLinkedin } from "react-icons/fa";
+import LogoYoutube from "../../../public/LogoYoutube.png";
+import LogoLinkedin from "../../../public/LogoLinkedin.png";
+import LogoSpotify from "../../../public/LogoSpotify.png";
 import dentinho from "/dentinho.png";
 import pipoca from "/pipoca_agil.png";
 import urso from "/logo_pipoca 1.png";
@@ -11,14 +13,14 @@ export default function Footer(){
     <footer className={styles.container}>
       <img src={dentesFooter} alt="Dentes correndo" className={styles.fotoFooter}/>
       <div className={styles.container__esquerda}>
-        <img src={urso} alt="Imagem de um urso" />
-        <a href="https://pipocaagil.com.br/" target="_blank"><img src={pipoca} alt="Logo do Pipoca Ágil" /></a>
+        <a className={styles.logoUrso} href="/" target="_blank"><img src={urso} alt="Logo Urso" /></a>
+        <a className={styles.logoPipoca}  href="https://pipocaagil.com.br/" target="_blank"><img src={pipoca} alt="Logo do Pipoca Ágil" /></a>
       </div>
       <div className={styles.container__central}>
-        <div>
-          <a href="https://open.spotify.com/show/5J1scP1l7m7kXK6v5RZS7J?si=27d5abb36c16408f&nd=1&dlsi=e313a2412f91422d"  target="_blank"><FaSpotify size={57} color="#1DB954"/></a>
-          <a href="https://www.youtube.com/@PipocaAgil" target="_blank"><FaYoutube size={57} color="red"/></a>
-          <a href="https://www.linkedin.com/company/pipoca-%C3%A1gil/"  target="_blank"><FaLinkedin size={57} color="blue"/></a>
+        <div className={styles.container__links}>
+          <a href="https://open.spotify.com/show/5J1scP1l7m7kXK6v5RZS7J?si=27d5abb36c16408f&nd=1&dlsi=e313a2412f91422d"  target="_blank"><img src={LogoSpotify} alt="Logo do Spotify ela redireciona para o podcast pipoca agil" /></a>
+          <a href="https://www.youtube.com/@PipocaAgil" target="_blank"><img src={LogoYoutube} alt="Logo do youtube ela redireciona para o canal do pipoca agil" /></a>
+          <a href="https://www.linkedin.com/company/pipoca-%C3%A1gil/"  target="_blank"><img src={LogoLinkedin} alt="Logo do linkedin ela redireciona para o perfil do pipoca agil " /></a>
         </div>
         <p>&copy; 2003 - 2024 Todos os direitos reservados.</p>
       </div>
