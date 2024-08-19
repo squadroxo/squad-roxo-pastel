525047a636438735386aa349be7ef9702f7a4e5f
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-08-19 19:52:13 -0300

    feat: ajustei responsividade do componente

--- src/components/ContainerMembro/index.tsx
+++ src/components/ContainerMembro/index.tsx
@@ -13,18 +13,18 @@ interface IContainerMembroProps {
 export default function ContainerMembro({ foto, funcao, linkLinkedin, nomeMembro, sobreMembro, primario }: IContainerMembroProps){
   return (
     <>
-      <div className={styles.container} style={{ flexDirection: primario ? "row" : "row-reverse" }}>
+      <div className={styles.container}>
         <div className={styles.container__containerFoto}>
           <img src={foto} alt={`Foto do(a) ${nomeMembro}`} className={styles.container__containerFoto__foto}/>
         </div>
         <div className={styles.container__containerInfos}>
           <div className={styles.container__containerInfos__pessoa}>
-            <div>
-              <h1 className={styles.container__containerInfos__pessoa__nome}>{nomeMembro}</h1>
-              <h2 className={styles.container__containerInfos__pessoa__funcao}>{funcao}</h2>
+            <div className={styles.container__containerInfos__pessoa__txt}>
+              <h1 className={styles.container__containerInfos__pessoa__txt__nome}>{nomeMembro}</h1>
+              <h2 className={styles.container__containerInfos__pessoa__txt__funcao}>{funcao}</h2>
             </div>
-            <a href={linkLinkedin} target="_blank">
-              <img src={iconLinkedin} alt="teste" className={styles.imagemLinkedin}/>
+            <a href={linkLinkedin} target="_blank" className={styles.container__containerInfos__pessoa__icon}>
+              <img src={iconLinkedin} alt="Logo Linkedin" className={styles.imagemLinkedin}/>
             </a>
           </div>
           <div className={styles.container__containerInfos__containerSobre}>
