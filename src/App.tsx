f242717ec4461168a1ed8ae51bbbaa76efc8e443
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-08-26 17:12:02 -0300

    fix: fixing size, weight and spacing

--- src/App.tsx
+++ src/App.tsx
@@ -11,10 +11,10 @@ import linhaDivisoria from "/linhaDivisoria.svg"
 function App() {
   return (
     <div className={styles.container}>
-      <ContainerRoxoPastel/>
+      {/* <ContainerRoxoPastel/> */}
       <ContainerSobre/>
       <img src={dentesWallpaper} alt="Wallpaper de dentes" className={styles.foto}/>
-      <h2 className={styles.tituloSquad}><strong>Membros</strong> da Squad</h2>
+      <h2 className={styles.tituloSquad}><strong className={styles.destaque}>Membros</strong> da Squad</h2>
       <div className={styles.containerAuxiliar}>
         <div className={styles.containerCardIbson}>
           <div className={styles.containerCardIbson__containerFoto}>
